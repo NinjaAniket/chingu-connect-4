aca74547a25a360ce9765c344cc105d258f7cf7c
NinjaAniket <aniketvnagapure@gmail.com>
2019-05-27 18:29:30 +0530

    Updates

---
+++ precache-manifest.e0723c98fd4bdf3e7f6c32ee847f301b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31dc81f444b66261f1ab26cb3c59fba",
+    "url": "/connect-4/index.html"
+  },
+  {
+    "revision": "bde10c5fb1682a3abec7",
+    "url": "/connect-4/static/css/main.65103bf0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/connect-4/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bde10c5fb1682a3abec7",
+    "url": "/connect-4/static/js/main.0ed21aea.chunk.js"
+  },
+  {
+    "revision": "988652424af3b2d832e7",
+    "url": "/connect-4/static/js/runtime~main.308217db.js"
+  }
+]);
